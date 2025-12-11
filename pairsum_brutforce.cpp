756766f16f8a088353b6288a2113758ad7c4d00b
Anubhav singh <anubhav.1406singh@gmail.com>
2025-12-11 11:53:45 +0530

    m

---
+++ pairsum_brutforce.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+vector<int>pairsum(vector<int> nums,int target){
+    int n=nums.size();
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+
+}
+int main(){
+    vector<int> nums={2,7,11,15};
+    int target=26;
+    vector<int> ans=pairsum(nums,target);
+    cout<<ans[0]<<" "<<ans[1]<<endl;
+}
