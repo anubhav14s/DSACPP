e78b74490acaf2c4bb9c2ad6c8527c37876f1247
Anubhav singh <anubhav.1406singh@gmail.com>
2025-12-16 10:59:19 +0530

    m

--- Rotated_array.cpp
+++ Rotated_array.cpp
@@ -1,38 +1,36 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-
-int rotated(vector<int> A, int tar) {
-    int st = 0, end = A.size() - 1;
-
-    while (st <= end) {
-        int mid = st + (end - st) / 2;
-
-        if (A[mid] == tar)
+int rotated(vector<int>& a,int tar){
+    int st=0,end=a.size()-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(a[mid]==tar){
             return mid;
-
-        // Left half sorted
-        if (A[st] <= A[mid]) {
-            if (A[st] <= tar && tar < A[mid])
-                end = mid - 1;
-            else
-                st = mid + 1;
         }
-        // Right half sorted
-        else {
-            if (A[mid] < tar && tar <= A[end])
-                st = mid + 1;
-            else
-                end = mid - 1;
+        if(a[st]<a[mid]){//LEFT PART SORTED
+            if(a[st]<=tar && a[mid]>=tar){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        else{//RIGHT PART SORTED
+            if(a[mid]<=tar && a[end]>=tar){
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
         }
     }
     return -1;
 }
-
-int main() {
-    vector<int> arr = {3, 4, 5, 6, 7, 0, 1, 2};
-    int target = 2;
-
-    int ans = rotated(arr, target);
-    cout << ans << endl;
+int main(){
+    vector<int> arr ={6,7,0,1,2,3,4,5};
+    int target=13;
+    int ans=rotated(arr,target);
+    cout<<ans; 
 }
+
