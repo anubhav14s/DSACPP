84cd64b6d0be40f4d88b3eb2d13a97c40540683d
Anubhav singh <anubhav.1406singh@gmail.com>
2026-01-02 17:01:55 +0530

    m

---
+++ insertion_sort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int curr=arr[i];
+        int prev=i-1;
+        while(prev>=0 && arr[prev]>curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+
+        arr[prev+1]=curr;
+    }
+}
+int printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n=5;
+    int arr[5]={4,1,3,5,2};
+    insertionSort(arr,n);
+    printArr(arr,n);
+    return 0;
+
+}
