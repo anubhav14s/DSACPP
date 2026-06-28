801a6865b0e1f166ee3ccd6362f9f663170026ba
Anubhav singh <anubhav.1406singh@gmail.com>
2026-06-28 21:54:22 +0530

    m

---
+++ RECURSION/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int factor(int n){
+    if(n==0||n==1) return 1;
+    return n*factor(n-1);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout<<factor(n);
+}
