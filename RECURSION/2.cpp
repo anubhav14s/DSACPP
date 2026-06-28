be4c500cadfef3dc09d419e0729793f96e677d2a
Anubhav singh <anubhav.1406singh@gmail.com>
2026-06-28 21:45:16 +0530

    m

---
+++ RECURSION/2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void summ(int i,int sum){
+    if(i<0) {
+        cout<<sum<<endl;
+        return;
+    }
+    summ(i-1,sum+i);
+}
+int main(){
+    int n;
+    cin>>n;
+    summ(n,0);
+}
