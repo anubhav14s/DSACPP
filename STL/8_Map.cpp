555e6d3c836bcd297004d52dbd4b4b15618caa0e
Anubhav singh <anubhav.1406singh@gmail.com>
2026-06-29 09:02:37 +0530

    m

--- STL/8_Map.cpp
+++ STL/8_Map.cpp
@@ -11,7 +11,6 @@ void explainmap(){
     for (int i = 1; i <= 5; i++) {
     mp.insert({i , i * 10});
   }
-
   cout << "Elements present in the map: " << endl;
   cout << "Key\tElement" << endl;
   for (auto it = mp.begin(); it != mp.end(); it++) {
