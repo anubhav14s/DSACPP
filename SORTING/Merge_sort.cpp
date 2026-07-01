a4c9f7da72ba59746ca7835d0f5c9961976d90c6
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-01 16:12:47 +0530

    m

--- SORTING/Merge_sort.cpp
+++ SORTING/Merge_sort.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+
+void merge(vector<int> v,int low,int mid,int high){
+
+}
+
+void ms(vector<int> v,int low,int high){
+    if (low>=high) return;
+    int mid = (low+high)/2;
+    ms(v,low,mid);
+    ms(v,mid+1,high);
+    merge(v,low,mid,high);
+}
+void mergesort(vector<int> &arr,int n){
+    ms(arr,0,n-1);
+}
+
+int main(){
+    
+}
