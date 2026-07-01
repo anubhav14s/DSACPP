8396785e988591734ef7ae02c202b6514ba1c470
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-01 15:55:31 +0530

    m

--- SORTING/Bubble_sort.cpp
+++ SORTING/Bubble_sort.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 void bubble(int arr[],int n){
     for (int i=0;i<n-1;i++){
+        int count=0;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                count++;
             }
         }
+        if(count==0) break;
     }
 
 }
