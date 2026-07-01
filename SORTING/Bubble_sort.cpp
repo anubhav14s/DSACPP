dd4946aa6041c1f08fe1060c54fc8e31869823f0
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-01 11:32:05 +0530

    m

--- SORTING/Bubble_sort.cpp
+++ SORTING/Bubble_sort.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void selection(int arr[],int n){
-    for (int i=n-1;i>1;i++){
-        int mini=i;
-        for(int j=i;j<=n-1;j++){
-            if(arr[j]<arr[mini]) mini=j;
+void bubble(int arr[],int n){
+    for (int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
-        swap(arr[i],arr[mini]);
     }
 
 }
@@ -18,7 +18,7 @@ int main(){
     for (int i=0;i<n;i++){
         cin>> arr[i];
     }
-    selection(arr,n);
+    bubble(arr,n);
     for (int i=0;i<n;i++){
         cout<< arr[i]<<" ";
     }
