9508cfea43557102dac968c474e9eedbda0d87fc
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-01 18:17:12 +0530

    m

--- SORTING/Merge_sort.cpp
+++ SORTING/Merge_sort.cpp
@@ -1,21 +1,51 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void merge(vector<int> v,int low,int mid,int high){
+void merge(vector<int> &v,int low,int mid,int high){
+    int left = low;
+    int right = mid+1;
+    vector<int> temp;
 
+    while(left<=mid&&right<=high){
+        if (v[left]<=v[right]){
+            temp.push_back(v[left]);
+            left++;
+        }
+        else{
+            temp.push_back(v[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(v[left]);
+            left++;
+    }
+    while(right<=high){
+        temp.push_back(v[right]);
+            right++;
+    }
+    for (int i=low ;i<=high;i++){
+        v[i]=temp[i-low];
+    }
 }
 
-void ms(vector<int> v,int low,int high){
+void ms(vector<int> &v,int low,int high){
     if (low>=high) return;
     int mid = (low+high)/2;
     ms(v,low,mid);
     ms(v,mid+1,high);
     merge(v,low,mid,high);
 }
-void mergesort(vector<int> &arr,int n){
-    ms(arr,0,n-1);
+void mergesort(vector<int> &v,int n){
+    ms(v,0,n-1);
 }
 
 int main(){
-    
+   
+    vector<int> v={3,1,2,4,1,5,6,7,4};
+    mergesort(v,v.size());
+    for(auto it=v.begin();it!=v.end();it++){
+        cout<<*it<<" ";
+    }
 }
