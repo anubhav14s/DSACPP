87d9c1eb2d6006697504ba4ee7a3f4547e515435
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-02 21:39:21 +0530

    m

--- SORTING/Quick_sort.cpp
+++ SORTING/Quick_sort.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int partation(vector<int> &v,int low ,int high){
-    int piviot=v[low];
+    int pivot=v[low];
     int i=low;
     int j=high;
     while(i<j){
-        while(v[piviot]>=v[i] && i<=high){
+        while(i <= high && v[i] <= pivot){
             i++;
         }
-        while(v[piviot]<v[j] && j>=low){
+        while(j >= low && v[j] > pivot){
             j--;
         }
         if(i<j){
@@ -24,7 +24,7 @@ void QuickSort(vector<int> &v,int low ,int high){
 
     if(low<high){
         int part=partation(v,low , high);
-    QuickSort(v,low,part+1);
+    QuickSort(v,low,part-1);
     QuickSort(v,part+1,high);
     }
 }
@@ -32,6 +32,7 @@ void QuickSort(vector<int> &v,int low ,int high){
 int main(){
     vector <int> v{1,3,2,4,7,9,5,8,6};
     QuickSort(v,0,v.size()-1);
+    
     for(int x:v){
         cout<<x<<" ";
     }
