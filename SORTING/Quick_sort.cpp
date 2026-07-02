a8f879b16f07ca66d37e119c1ea2dc27774cfa77
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-02 21:29:15 +0530

    m

--- SORTING/Quick_sort.cpp
+++ SORTING/Quick_sort.cpp
@@ -2,6 +2,39 @@
 #include<vector>
 using namespace std;
 
-void QuickSort(vector<int> &v,int n){
-    
+int partation(vector<int> &v,int low ,int high){
+    int piviot=v[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(v[piviot]>=v[i] && i<=high){
+            i++;
+        }
+        while(v[piviot]<v[j] && j>=low){
+            j--;
+        }
+        if(i<j){
+            swap(v[j],v[i]);
+        }
+    }
+    swap(v[low],v[j]);
+    return j;
+}
+void QuickSort(vector<int> &v,int low ,int high){
+
+    if(low<high){
+        int part=partation(v,low , high);
+    QuickSort(v,low,part+1);
+    QuickSort(v,part+1,high);
+    }
+}
+
+int main(){
+    vector <int> v{1,3,2,4,7,9,5,8,6};
+    QuickSort(v,0,v.size()-1);
+    for(int x:v){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
