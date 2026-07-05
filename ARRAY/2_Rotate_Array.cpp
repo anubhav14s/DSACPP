40e5d3a8b80570287725cfd030e1f610b0270ad4
Anubhav singh <anubhav.1406singh@gmail.com>
2026-07-05 17:54:17 +0530

    m

---
+++ ARRAY/2_Rotate_Array.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void rotateright(int arr[],int n,int d){
+    d=d%n;
+    if (d==0) return ;
+    int temp[d];
+    for(int i=n-d;i<n;i++){
+        temp[i-(n-d)]=arr[i];
+    }
+    for(int i=n-d-1;i>=0;i--){
+        arr[i+d]=arr[i];
+    }
+    for(int i=0;i<d;i++){
+        arr[i]=temp[i];
+    }
+}
+void rotateleft(int arr[],int n,int d){
+    d=d%n;
+    if (d==0) return ;
+    int temp[d];
+    for(int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    for(int i=d;i<n;i++){
+        arr[i-d]=arr[i];
+    }
+    for(int i=0;i<d;i++){
+        arr[n-d+i]=temp[i];
+    }
+
+}
+int main(){
+    int arr[6]={2,9,5,3,1,7};
+    rotateleft(arr,6,29);
+    rotateright(arr,6,3);
+    for(int x:arr){
+        cout<<x<<" ";
+    }
+}
